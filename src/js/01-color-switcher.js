a8c3e307059ffe98869114429d83bf9ed33272f8
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-02-05 22:27:21 +0200

    Adds solving the task-01 color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+import '../css/common.css';
 
+const body = document.querySelector('body');
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let timerID = null;
+buttonStop.disabled = true;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  timerID = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true;
+  buttonStop.disabled = false;
+}
+
+function stopChangeColor() {
+  clearInterval(timerID);
+  body.style.backgroundColor = '#fafafa';
+  buttonStart.disabled = false;
+  buttonStop.disabled = true;
+}
+
+buttonStart.addEventListener('click', changeColor);
+buttonStop.addEventListener('click', stopChangeColor);
