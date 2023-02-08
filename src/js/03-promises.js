8cd3be5a088d14820e99a203fa48b149e682e3d0
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-02-08 10:37:30 +0200

    Adds promises to task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,10 +1,35 @@
 import '../css/common.css';
 
-function createPromise(position, delay) {
+const form = document.querySelector('.form');
+
+const createPromise = (position, delay) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+};
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = event.currentTarget;
+
+  for (let i = 0; i < Number(amount.value); i += 1) {
+    createPromise(i + 1, Number(delay.value) + Number(step.value) * i)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
 }
+
+form.addEventListener('submit', handleSubmit);
